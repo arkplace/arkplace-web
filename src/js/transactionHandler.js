8c4ca676eefcdf0f2dda4817b34a6235df228550
Moazzam Abdullah Khan <moazzam2k@yahoo.com>
2020-04-15 20:45:40 +0200

    feat: get all transactions for address

--- src/js/transactionHandler.js
+++ src/js/transactionHandler.js
@@ -2,6 +2,10 @@ import { APIRequestHandler } from "./apiRequestHandler.js";
 import { EndpointHandler } from "/src/js/endpointHandler.js";
 import { PeerHandler } from "/src/js/peerHandler.js";
 
+const isEmpty = (obj) => {
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
 export class TransactionHandler {
     constructor(walletId, getOutgoing = true, getIncoming = false) {
         this.peerHandler_ = new PeerHandler();
@@ -17,17 +21,75 @@ export class TransactionHandler {
 
         this.readyState = false;
         this.lastSeenTimestamp = null;
+        this.lastSeenCount = 0;
+        this.pageNumber = 1;
+        this.txQueue = [];
+        this.lastTx = {};
+    }
+    
+    syncTransactionHistory() {
+        this.updateTransactions(this.appendTransactionsToHistory);
     }
 
-    syncTransactionHistory() {
-        
-        this.getOutgoingTransactions();
+    isValidNewData(JsonTxData) {
+        if ( !JsonTxData )
+        {
+            return false;
+        }
+        if ( JsonTxData.meta.totalCount == this.lastSeenCount ) {
+            return false;
+        }
+        return true;
+    }
+
+    isUnseenTransaction(tx) {
+        return tx.timestamp.epoch > this.lastSeenTimestamp;
+    }
+
+    isIncomingTx(tx) {
+        return this.walletId == tx.recipient;
+    }
+
+    isOutgoingTx(tx) {
+        return this.walletId == tx.sender;
+    }
+
+    appendTransactionsToHistory(JsonTxData) {
+        if (!this.isValidNewData(JsonTxData)) {
+            return;
+        }
+
+        var wasAnythingAdded = false;
+        for (var idx in JsonTxData.data) {
+            var tx = JsonTxData.data[idx];
+            if (this.isUnseenTransaction(tx)) {
+                if (this.shouldStoreIncoming && this.isIncomingTx(tx)) {
+                    this.txQueue.push(tx);
+                    wasAnythingAdded = true;
+                }
+                else if (this.shouldStoreOutgoing && this.isOutgoingTx(tx)) {
+                    this.txQueue.push(tx);
+                    wasAnythingAdded = true;
+                }
+                if (!isEmpty(tx)) {
+                    this.lastTx = tx;
+                }
+            }
+        }
+
+        if (wasAnythingAdded) {
+            this.syncTransactionHistory();
+        }
+        else {
+            this.lastSeenTimestamp = this.lastTx.timestamp.epoch;
+            this.lastSeenCount = JsonTxData.meta.totalCount;
+        }
     }
 
     initializeReadyState() {
+        this.readyState = true;
         this.loadNextPeer();
         this.syncTransactionHistory();
-        this.readyState = true;
     }
 
     loadSeedPeers() {
@@ -38,36 +100,23 @@ export class TransactionHandler {
         this.peerToConnect_ = this.peerHandler_.getRandomPeer();
     }
 
-    getOutgoingTransactions(cb = null) {
-        this.transactionReceivedCallback_ = cb;
-        this.getTransactions("outgoing");
-    }
-
-    getIncomingTransactions(cb = null) {
-        this.transactionReceivedCallback_ = cb;
-        this.getTransactions("incoming");
-    }
-
-    updateTransactions(mode) {
+    updateTransactions() {
         var peerURI = this.peerHandler_.convertToURI(this.peerToConnect_)
-                    + EndpointHandler.getSearchTransactionsAPIEndpoint();
-
+                    + EndpointHandler.getSearchTransactionsAPIEndpoint()
+                    + EndpointHandler.getArgSpecifier()
+                    + EndpointHandler.getPageIndexFormatted(this.pageNumber);
+        
         if (this.shouldStoreIncoming) {
             APIRequestHandler.sendJSONRequest(peerURI,
-                this.transactionSearchJSONReceived,
+                this.appendTransactionsToHistory.bind(this),
                 EndpointHandler.createSearchRequestPOSTData( null, this.walletId, this.lastSeenTimestamp));
         }
         if (this.shouldStoreOutgoing) {
             APIRequestHandler.sendJSONRequest(peerURI,
-                this.transactionSearchJSONReceived,
+                this.appendTransactionsToHistory.bind(this),
                 EndpointHandler.createSearchRequestPOSTData( this.walletId, null, this.lastSeenTimestamp));
         }
+        this.pageNumber++;
     }
 
-    transactionSearchJSONReceived(data) {
-        console.log(data);
-        if (this.transactionReceivedCallback_) {
-            this.transactionReceivedCallback_(data);
-        }
-    }
 };
